172fa8cea17c965acae35c10a2309e8446ab54cd
Alden G <aldengrover@gmail.com>
2026-05-01 12:01:52 -0400

    update info.yaml and testbench

--- src/tt_um_sandsim.sv
+++ src/tt_um_sandsim.sv
@@ -357,7 +357,7 @@ module spi
     end
     endmodule: spi
 
-module tt_um_example (
+module tt_um_sandsim_Alden-G878 (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
